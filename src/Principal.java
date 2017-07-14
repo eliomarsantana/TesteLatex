33b072e8a90dba709d0470e1b4cb2b30f7c30985
Eliomar Santana <esantana.ti@gmail.com>
2017-07-14 08:33:24 -0300

    correcao codificacao com novo metodo

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -21,21 +26,23 @@ class Principal {
 		
 		
 		
-		String dados = new String(Files.readAllBytes(new File("C:\\Users\\eliom\\Documents\\tcc\\main.tex").toPath()),Charset.forName("UTF-8").displayName());
+		//String dados = new String(Files.readAllBytes(new File("C:\\Users\\eliom\\Documents\\tcc\\main.tex").toPath()),Charset.forName("UTF-8").displayName());
 
-		/*TESTAR ISSO
-		Charset cs = Charset.forName("UTF-8"); 
-		String dados = new String(Files.readAllBytes(new File("/home/eliomar/TCC-ADS-EliomarSantana/main.tex").toPath()), cs);
-		OU ISSO
-		Charset.forName("UTF-8").encode(dados);
-		*/
+		//TESTAR ISSO
+		//Charset cs = Charset.forName("ISO-8859-1"); 
+		String dados = new String(Files.readAllBytes(new File("C:\\Users\\eliom\\Documents\\tcc\\main.tex").toPath()));
+		//System.out.println(dados);
+		//OU ISSO
+		//Charset.forName("ISO-8859-1").decode(dados);
+		
+		Principal principal = new Principal();
 		
 		//BufferedReader arqIn = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\eliom\\Documents\\tcc\\main.tex"), "UTF-8"));
 		
 		int count = 0;
 		String regex = "\\\\input\\{(?<conteudo>.*?)\\}";
 
-		String title = dados.replace("\\", "\\\\");//escapando as barras no arquivo
+		String title = principal.charset(dados).replace("\\", "\\\\");//escapando as barras no arquivo
 		
 		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		Matcher comparator = pattern.matcher(title);
@@ -44,10 +51,9 @@ class Principal {
 			title = comparator.group("conteudo");
 			System.out.println(title);
 			String conteudo = new String(Files.readAllBytes(new File("C:\\Users\\eliom\\Documents\\tcc\\"+title).toPath()));
-			//System.out.println("conteudo: "+conteudo);
+
 			
-			
-			String t = conteudo.replaceAll("%", "");
+			String t = principal.charset(conteudo).replaceAll("%", "");
 			String t2 = t.replaceAll("&", "");
 			org.jbibtex.LaTeXParser latexParser = new org.jbibtex.LaTeXParser();
 
@@ -61,34 +67,23 @@ class Principal {
 			
 		}
 		
+	
+	}
+	public String charset(String texto) throws CharacterCodingException{
 		
-		
-		
-		
-		System.out.println("\n\n\n\n\n\n\n");
-		/*String latexString = "% "
-				+ "\\section{A solução: Corretor para Trabalhos de Conclusão de Curso}\\label{sec:a-solucao} "
-				+ "Este trabalho tem como objetivo principal o desenvolvimento de um corretor automático de trabalhos de "
-				+ "conclusão de curso. A ferramenta foi modelada de forma extensível para que desse suporte a diferentes tipos "
-				+ "e formatos de arquivos de entrada. Entretanto, no escopo deste projeto, foi implementado a solução para correção "
-				+ "de trabalhos escritos em formato .tex. \\vskip 1em "
-				+ "e realizada a validação da mesma. Do ponto de vista da implementação, seguimos os seguintes passos: análise de requisitos, "
-				+ "definição da arquitetura, modelagem de classes, implementação, e testes. Esses passos são descritos nas subseções seguintes.\\vskip 1em "
-				+ "\\subsection{Análise de Requisitos}\\vskip 1em Segundo \\cite{KOTONYA:SOMMERVILLE}, a análise de requisitos tem por objetivo tratar do processo "
-				+ "de definição dos requisitos de software. Para isso, todas as atividades de desenvolvimento precisam ser criteriosamente elaboradas e desenvolvidas. Com isso, foi de suma importância fazer uma análise de requisitos para saber o escopo mais próximo do real que a ferramenta irá atingir.\\vskip 1em ";
+		Charset charsetE = Charset.forName("ISO-8859-1");
+		CharsetEncoder encoder = charsetE.newEncoder();
 
-				*/
-				
-				
-				
-				
-				//Hyphenator teste = new Hyphenator();
-				
-				//teste.setErrorHandler(new MyErrorHandler());
-				//teste.loadTable(new java.io.BufferedInputStream(new java.io.FileInputStream("/home/eliomar/tcceliomar/main.tex")));
-				//String hyphenated_word=teste.hyphenate("palavra");
-				
-				//System.out.println("------------->>> "+hyphenated_word);
+		//i believe from here to the end will probably stay the same, as per your posted example.
+		Charset charsetD = Charset.forName("UTF-8");
+		CharsetDecoder decoder = charsetD.newDecoder();
+
+		ByteBuffer bbuf = encoder.encode(CharBuffer.wrap(texto));
+		CharBuffer cbuf = decoder.decode(bbuf);
+		//final String result = cbuf.toString();
 		
+		
+		
+		return cbuf.toString();
 	}
 }
